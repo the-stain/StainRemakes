e11af79340d7f2c3b09a182a683dad3c86406c47
the-stain <49831079+the-stain@users.noreply.github.com>
2021-05-09 11:16:24 -0400

    added comments

--- Assets/DragAndDrop.cs
+++ Assets/DragAndDrop.cs
@@ -4,26 +4,52 @@ using UnityEngine;
 using UnityEngine.Tilemaps;
 using UnityEngine.UI;
 
+/*
+ * This abomination of a script (attached to the WeatherNodeMarker object) handles the interaction/navigation of the node through the maze as well 
+ * as the procedure for solving it, with a pinch of maze setup. Like a little code salad.
+ */
+
 public class DragAndDrop : MonoBehaviour
 {
+    // Used for judging if the mouse position is in a cell adjacent to the node's position
     bool mousePosA = false;
-    Vector2Int nodePosInt = new Vector2Int();
-    Vector2Int mousePosInt = new Vector2Int();
+
+    // The highlight tiles mark the trail created by dragging the node through the maze
+    public Tile highlight;
+    public Tile wallBlock;
     public Tilemap targetGrid;
-    //Vector3 worldPos;
-    //Vector3 screenCursorPos;
+
+    // Used for when the maze is completed
+    public Image blockerPanel;
+    // I don't know why I put this here, but... here it is :|
+    public Text finishText;
+    
+    // Used to define a "valid area" on the tilemap that the node is supposed to stay in
+    BoundsInt mazeBounds;
+
+    // Because the method needed to get the mouse's position returns a Vector2, this variable holds it so it can be converted into a more usable Vector2Int
     Vector2 mousePos = new Vector2();
-    readonly Vector2Int[] nodeDirections = new[] { new Vector2Int(), new Vector2Int(), new Vector2Int(), new Vector2Int() };
+
+    // The coordinates in which the node currently sits
+    Vector2Int nodePosInt = new Vector2Int();
+
+    // The coordinates in which the mouse cursor sits (when this doesn't match nodePosInt, the algorithm then determines if this is a valid target for the node)
+    Vector2Int mousePosInt = new Vector2Int();
+
+    // This keeps track of the cells directly adjacent to the node
     Vector2Int[] adjacentCells = new[] { new Vector2Int(), new Vector2Int(), new Vector2Int(), new Vector2Int() };
+
+    // This is used to calculate what the adjacentCells are
+    readonly Vector2Int[] nodeDirections = new[] { new Vector2Int(), new Vector2Int(), new Vector2Int(), new Vector2Int() };
+
+    // Full disclosure: I don't remember exactly why I used Vector3Ints for these instead of Vector2Ints. I think it had something to do with the methods returning
+    // Vector3Ints and I couldn't just cast them into Vector2Ints or something
     Vector3Int snapPos = new Vector3Int();
     Vector3Int targetPos = new Vector3Int(17, 0, 0);
     Vector3Int startPos = new Vector3Int(1, 6, 0);
-    public Tile highlight;
-    BoundsInt mazeBounds;
-    public Image blockerPanel;
-    public Text finishText;
-    public Tile wallBlock;
 
+    // The nodeDirections array has to be initialized and the area of the mazeBounds is set (think of it as a rectangle with the bottom-left corner being the 
+    // first variable and the top-right being the second). SetNode is also called here for some reason that I'm sure was useful.
     private void Start()
     {
         nodeDirections[0] = Vector2Int.up;
@@ -34,26 +60,42 @@ public class DragAndDrop : MonoBehaviour
         SetNode();
     }
 
+    // Rather self-explanatory. Sets the node to the starting position of the maze (which is needed when the maze is solved or the player stops dragging the node)
+
     public void SetNode()
     {
         transform.position = targetGrid.GetCellCenterWorld(startPos);
     }
+
+    // Where all the navigation stuff is done. As soon as this is triggered, the cell at the starting position (where the node is already sitting) is highlighted
+    // and nodePosInt and mousePosInt are given their respective coordinates. Then the adjacentCells array is filled by using nodeDirections to calculate the cells 
+    // adjacent from nodePosInt. Now the algorithm checks for whether the mouse and node positions match (since this is in OnMouseDrag() this whole sequence is 
+    // called every frame that the mouse is held down). When they don't, the mazeBounds (the boundary rectangle made for the maze) is checked to make sure that the
+    // mouse is still in it. If it is, then the adjacentCells array is checked to make sure that the mouse position matches one of them; if it does match, then one
+    // more check needs to be done to make sure that the mouse position's cell doesn't have a tile on it (the node can only move into empty cells). If it passes 
+    // this, then finally the node can be moved to the mouse's position and that position can be highlighted.
     void OnMouseDrag()
     {
         targetGrid.SetTile(startPos, highlight);
         nodePosInt = (Vector2Int)targetGrid.WorldToCell(transform.position);
+        // because ScreenToWorldPoint() gives a position from a different "space" than the grid, we have to use WorldToCell() to convert it to actual coordinates
         mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         mousePosInt = (Vector2Int)targetGrid.WorldToCell(mousePos);
+        // this is where the cells adjacent to the current one (nodePosInt) are calculated
         for (int i = 0; i < adjacentCells.Length; i++)
         {
+            // since nodeDirections consists of up/down/left/right, adding each one to nodePosInt gives the adjacent cell in that direction.
             adjacentCells[i] = nodePosInt + nodeDirections[i];
         }
+        // if this is true, then the mouse cursor has moved off of the node's cell and we start evaluating the new cell to see if it's valid
         if (mousePosInt != nodePosInt)
         {
             Debug.Log(mousePosInt);
+            // this checks the mazeBounds thing to see if the new cell would be inside of it
             if (mazeBounds.Contains((Vector3Int)mousePosInt))
             {
                 Debug.Log("passed Contains");
+                // this checks to make sure that the new cell is one of the adjacentCells
                 foreach (Vector2Int a in adjacentCells)
                 {
                     if (mousePosInt == a)
@@ -61,19 +103,29 @@ public class DragAndDrop : MonoBehaviour
                 }
                 if (mousePosA)
                 {
+                    // this is where I take mousePosInt and put it into a Vector3Int. I don't remember why I did this, but I can only assume I needed to
                     snapPos = (Vector3Int)mousePosInt;
+                    // this checks to make sure that the new cell doesn't have a tile occupying it already; if it does, then it's a block, a wall, or a path which
+                    // would never be valid targets for obvious reasons
                     if (!targetGrid.HasTile(snapPos))
                     {
+                        // this is where, if the new cell passes all of the checks, the node is moved to that position and the cell is highlighted
+                        // I THINK this is why I needed a Vector3Int (because transform.position needs it), but don't quote me on that I don't remember
                         transform.position = targetGrid.GetCellCenterWorld(snapPos);
                         targetGrid.SetTile(snapPos, highlight);
                         //Debug.Log("snapPos is " + snapPos.ToString());
                     }
+                    // then this is set back to false in case it was changed
                     mousePosA = false;
                 }
 
             }
 
         }
+        // If this is true, then the node is at the end of the maze and the maze completion stuff can begin.
+        // Technically, checking snapPos isn't ideal because snapPos is set to the new cell's position before it's cleared as a valid target.
+        // If there was a tile on the new cell, then that cell could be invalidated; but if that new cell was targetPos, then the if statement below would 
+        // still be true. However, because the WeatherNodeGen prevents a tile from being put on targetPos, this isn't a problem for us. 
         if (snapPos == targetPos )
         {
             CompleteMaze();
@@ -81,6 +133,9 @@ public class DragAndDrop : MonoBehaviour
 
     }
 
+    // This is triggered when the player gets the node to the targetPos. A coroutine (basically a method that can be run more "slowly" than normal)
+    // plays that displays a "completed" message, the maze is reset and a new one is generated, and snapPos is set to (0,0,0) to make sure that 
+    // the OnMouseDrag() method has a "clean slate" to work with (I think)
     private void CompleteMaze()
     {
         Debug.Log("target reached!");
@@ -91,22 +146,38 @@ public class DragAndDrop : MonoBehaviour
         snapPos.Set(0, 0, 0);
     }
 
+    // This is triggered when the player lets go of the node they are dragging. It checks to see if the node is at the maze endpoint and if it's
+    // not, it sets the node back to the starting position and clears any highlighted tiles
     private void OnMouseUp()
     {
+        // if OnMouseUp is being triggered, then this is basically always true because CompleteMaze() would be triggered if snapPos == targetPos
         if(snapPos != targetPos)
         {
+            // I realize as I'm writing of these comments that I could've called SetNode() instead of writing this out, but I didn't 
+            // probably because I wrote SetNode() after writing this thing and forgot about it
             transform.position = targetGrid.GetCellCenterWorld(startPos);
             targetGrid.SwapTile(highlight, null);
         }
     }
 
+    // This is the coroutine being called in CompleteMaze(). While a normal method runs every instruction as quickly as possible, this
+    // can be "slowed down" to run in a certain timeframe. It can be used to make animations without having to use Unity's animation
+    // system, or it can be used with animations like I did here to create sequences (kind of like cutscenes), or other stuff I'm sure.
+    // Here, a currently-invisible UI panel that normally sits over the maze becomes a raycast target, which effectively blocks clicks 
+    // from reaching anything underneath it. Then, an animation on that panel is triggered that notifies the player that they've properly
+    // solved the maze. Only when that animation is complete does the trigger reset the animator and the panel becomes unclickable again.
     IEnumerator CompleteNodeDropDown()
     {
         Debug.Log("COROUTINE");
+        // there's a couple of ways that a panel like this can be handled, but in this case this way worked best IMO
         blockerPanel.raycastTarget = true;
+        // the panel's animator begins in the "neutral" state, and this causes it to move to the PanelFadeIn state (which is where the animation is)
         blockerPanel.GetComponent<Animator>().SetTrigger("CompleteNode");
+        // this tells the coroutine to wait until the panel's raycastTarget is disabled, which happens at the very end of the animation clip
         yield return new WaitUntil(() => blockerPanel.raycastTarget == false);
+        // this moves the animator back to the neutral state so it's in the right place the next time this coroutine happens
         blockerPanel.GetComponent<Animator>().SetTrigger("CompleteNode");
+        // this makes extra super duper sure that this is set to false again
         blockerPanel.raycastTarget = false;
     }
 
--- Assets/WeatherNodeGen.cs
+++ Assets/WeatherNodeGen.cs
@@ -3,21 +3,42 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
+/* 
+ * This handles all the procedural generation for the maze. It uses a Tilemap and a variation of a passage-carving maze gen algorithm to create a path of tiles, 
+ * then add walls at random on the appropriate tiles. The result is a maze that can have multiple solutions, isolated areas, dead ends, loops, and junctions -- 
+ * just like the original WN puzzle from Among Us.
+ */
+
 public class WeatherNodeGen : MonoBehaviour
 {
-    Vector2Int currentCell = new Vector2Int(1,6); // the coordinates of the cell being evaluated. Used by both GenPath and PlaceWalls
-    Vector2Int[] pathDirections = new Vector2Int[] { new Vector2Int(), new Vector2Int(), new Vector2Int(), new Vector2Int() } ;// the directions in which the currentCell can move
-    int indexRand; // used by GenPath to determine the next direction as well as WallPlace to determine whether or not to place down a wall
-    Vector2Int nextCell01 = new Vector2Int(); // this and nextCell02 are compared against the pathTiles List to make sure that the next move doesn't cross over a previously traveled cell
-    Vector2Int nextCell02 = new Vector2Int(); 
-    bool cantGoLeft;
+
+    // The coordinates of the cell being evaluated, used by both GenPath and PlaceWalls. It's initialized to the starting position in the maze
+    Vector2Int currentCell = new Vector2Int(1,6);
+
+    // The directions in which the currentCell can move. Normally it is just up[0] down[1] left[2] and right[3], but can be changed depending on the currentCell's position.
+    Vector2Int[] pathDirections = new Vector2Int[] { new Vector2Int(), new Vector2Int(), new Vector2Int(), new Vector2Int() } ;
+
+    // Used by GenPath to determine the next direction as well as WallPlace() to determine whether or not to place down a wall. In GenPath(), the range is 0-3; in WallPlace(),
+    // the range is 0-10. Having these methods share a variable for randomization is probably bad practice because the ranges are different, but in this case I don't give a fuck
+    int indexRand;
+
+    // The list of all coordinates in the grid that are designated as path cells for the current maze.
     List<Vector2Int> pathCells = new List<Vector2Int>();
+
+    // This and nextCell02 are used in GenPath. Because of the maze's configuration, moves have to be made in increments of two blocks; both of these cells need to be compared
+    // against the pathCells list to make sure that they don't cross over any pathTiles
+    Vector2Int nextCell01 = new Vector2Int(); 
+    Vector2Int nextCell02 = new Vector2Int();
+
+    // If this is true, then the coordinates of the randomly chosen index needs to be checked to make sure it isn't moving left
+    bool cantGoLeft;
+
+    // Used by GenPath() and WallPlace() to determine if the evaluated cell is valid
     bool isValidCell;
+
     public Tile wallV;
     public Tile wallH;
     public Tile path;
-
-    List<Vector2Int> wallCells = new List<Vector2Int>();
 
     // Start is called before the first frame update
     void Start()
@@ -25,6 +46,9 @@ public class WeatherNodeGen : MonoBehaviour
         SetDirections();
     }
 
+
+    // This array needs to be set not only on Start(), but also at the end of every GenPath() call (in case it was altered). The words up/down/left/right are just
+    // shorthand for (0,1)/(0,-1)/(-1,0)/(1,0).
     public void SetDirections()
     {
         pathDirections[0] = Vector2Int.up;
@@ -33,17 +57,23 @@ public class WeatherNodeGen : MonoBehaviour
         pathDirections[3] = Vector2Int.right;
     }
 
+    // Rather self-explanatory. While other properties (like indexRand) will most likely be changed from their initial values, resetting those values is only important for 
+    // these ones here -- others can just be written over as needed.
     public void ResetMaze()
     {
         currentCell.x = 1;
         currentCell.y = 6;
         SetDirections();
         pathCells.Clear();
-        wallCells.Clear();
         GetComponent<Tilemap>().SwapTile(path, null);
         GetComponent<Tilemap>().SwapTile(wallH, null);
         GetComponent<Tilemap>().SwapTile(wallV, null);
     }
+
+    // The star of the show. ResetMaze() is always called first to ensure that the algorithm is working with a clean slate. It immediately adds the starting position 
+    // cell (1,6) to pathCells, moves down to (1,5), and adds that cell. Then it begins using GenPath() to carve a path to the end position (17,0). The way GenPath() is 
+    // written ensures that it will eventually reach the furthest column which is directly above the end position. At that point, the algorithm stops using GenPath()
+    // and simply carves downward until it reaches (17,0). After the path is complete, PlaceWalls() is called to... place walls.
 
     public void GenMaze()
     {
@@ -67,21 +97,36 @@ public class WeatherNodeGen : MonoBehaviour
         //}
     }
 
+    // This is where the maze gen magic actually happens. (Buckle in)
+    // First, the the currentCell's coordinates are checked to see if the pathDirections array should be altered. While the array doesn't absolutely need to change,
+    // doing so helps the next step (randomly choosing a direction) avoid rolling a direction that is always invalid. When a direction is rolled, the next two
+    // coordinates are calculated and then compared against the list of pathCells to see if either of them match any of the ones in the list. If neither nextCells01
+    // or nextCell02 match the pathCells list, then the cells are valid and they're both added to the list (and currentCell being updated to match). Whether or not 
+    // the cells are valid, GenPath() then ends by resetting the booleans used and calling SetDirections() to reset the pathDirections array.
+
     void GenPath()
     {
+
+        // If this condition is true, then the currentCell is at the bottom-most row and would never be able to move down; thus, the index for "down" is replaced with "up".
         if (currentCell.y == 1)
             pathDirections[1] = Vector2Int.up;
 
+        // If this is true, then currentCell is at the top row; thus, the index for "up" is replaced with "down".
         if (currentCell.y == 5)
             pathDirections[0] = Vector2Int.down;
 
+        // If this is true, then currentCell is at the left-most column; thus, the index for "left" is replaced with "right".
         if (currentCell.x == 1)
             pathDirections[2] = Vector2Int.right;
 
+        // If the currentCell has any of these coordinates, then turning left would result in an unsolvable maze.
+        // As I write these comments weeks after this code, I realize that I could've just replaced the "left" index of pathDirections with something else. But I didn't.
+        // I don't remember why I did it like this, but I'll be damned if I so much as breathe on any of this code right now so here it will remain.
         if (currentCell.y == 1 || currentCell.y == 5 || currentCell.x == 1)
         {
             cantGoLeft = true;
         }
+
         indexRand = Random.Range(0, pathDirections.Length);
         nextCell01 = currentCell + pathDirections[indexRand];
         nextCell02 = nextCell01 + pathDirections[indexRand];
@@ -110,8 +155,22 @@ public class WeatherNodeGen : MonoBehaviour
         SetDirections();
     }
 
+    // Now that the path has been generated, walls can be placed. The algorithm iterates through each cell (starting from the bottom left of the grid, by going across 
+    // a whole row before moving up the next column each time) and first checks if it's a path cell. If it doesn't, then it can check if it's a valid target for a wall.
+    //
+    // Note that walls can only be placed on cells that are directly adjacent to block tiles. Because the placement of the blocks is uniform, I can use a simple 
+    // equation to determine this. On my grid, blocks are at every cell that has an even-numbered x AND y coordinate; so for walls to be directly adjacent, they have to
+    // have at one coordinate that is also even. This means that walls can never be placed at cells that have an odd-numbered x AND y coordinate. So as long as the
+    // currentCell has one even coordinate, it's valid.
+    // Now the currentCell has to be checked to see which coordinate is even-numbered (since they will always have one odd and one even coordinate). There are two
+    // types of wall tiles: horizontal and vertical. When it's determined which wall should be used, then the WallPlace() method is called with that tile. 
+    // Note that this doesn't guarantee that a wall will be placed; WallPlace() is what actually makes that decision.
+
     public void PlaceWalls()
     {
+        // the maze is 19 x 7 cells, starting at (0,0)
+        // technically, the 6 and 18 in these loops should be 7 and 19 respectively, but I didn't notice until I wrote this comment weeks after finishing the code
+        // and I remain adamant in refusing to touch any of this shit -- just be aware that they should be 7 and 19 in order to replicate the original WN maze.
         for (int row = 0; row < 6; row++)
         {
             for (int col = 0; col < 18; col++)
@@ -125,25 +184,33 @@ public class WeatherNodeGen : MonoBehaviour
                         isValidCell = false;
                     }
                 }
+                // this checks if both the x and y coordinates are odd-numbered (the percentage meaning that the result is the remainder after division)
                 if (currentCell.x % 2 == 0 && currentCell.y % 2 == 0)
                 {
                     isValidCell = false;
                 }
+                // this is where the type of wall tile is determined
                 if (isValidCell)
                 {
+                    // if the currentCell has an even y coordinate, it uses the horizontal tile
                     if (currentCell.y % 2 == 0)
                     {
                         WallPlace(wallH);
                     }
+                    // if the currentCell has an even x coordinate, it uses the vertical tile.
+                    // Technically, the if statement doesn't need to be here, since if the previous one is false then this will always be true, but I just
+                    // liked the uniformity. And also didn't realize it until I wrote this comment.
                     else if (currentCell.x % 2 == 0)
                     {
                         WallPlace(wallV);
                     }
 
+                    // this didn't need to be here but... here it is <_<
                     isValidCell = true;
                 }
                 else
                 {
+                    // after everything is done, this is reset in case it was set to false
                     isValidCell = true;
                 }
             }
@@ -152,6 +219,9 @@ public class WeatherNodeGen : MonoBehaviour
         }
     }
 
+    // When PlaceWalls() has determined that its currentCell is a valid wall target, it calls this method (passing the appropriate wall tile for it to add.
+    // indexRand is assigned a random integer from 0-10 and if it is 2 or greater, it puts the corresponding wall tile on the currentCell. While I could've 
+    // just used 2 numbers (since it's a yes/no question), I wanted it to be more likely that it would place a wall than not -- hence the larger range.
     void WallPlace(Tile wall)
     {
         indexRand = Random.Range(0, 10);
